bb2d79382ed18e5e74eee25c039f4e0a1675b8e1
arshiamasih <arshia.masih@gmail.com>
2018-06-26 09:43:29 -0700

    edited candidate

--- script.js
+++ script.js
@@ -22,15 +22,15 @@ var createCandidate = function (name, partyColor) {
 };
 
 
-var candyse = createCandidate("Candyse Frazier", [132, 17, 11])
-var arshia = createCandidate("Arshia Masih", [245, 141, 136])
+var saundra = createCandidate("Saundra S.", [132, 17, 11])
+var arshia = createCandidate("Arshia M.", [245, 141, 136])
 
-candyse.results = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+saundra.results = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
 arshia.results = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
 
-candyse.results[9] = 1;
-candyse.results[4] = 17;
-candyse.results[43] = 11;
+saundra.results[9] = 1;
+saundra.results[4] = 17;
+saundra.results[43] = 11;
 
 arshia.results[9] = 28;
 arshia.results[4] = 38;
@@ -39,11 +39,11 @@ arshia.results[43] = 27;
 
 var setStateResults = function(state) {
 theStates[state].winner = null;
-  if (arshia.results[state] > candyse.results[state]) {
+  if (arshia.results[state] > saundra.results[state]) {
     theStates[state].winner = arshia;
     }
-  else if (arshia.results[state] < candyse.results[state]) {
-    theStates[state].winner = candyse;
+  else if (arshia.results[state] < saundra.results[state]) {
+    theStates[state].winner = saundra;
     }
 
   console.log("And the winner is " + theStates[state].winner + "of that " + state +"!!!");
@@ -72,17 +72,17 @@ var winnerName = body.children[2].children[1];
 
 stateName.innerText = theStates[state].nameFull;
 stateAbbrev.innerText = theStates[state].nameAbbrev;
-candidate1Name.innerText = candyse.name;
+candidate1Name.innerText = saundra.name;
 candidate1Results.innerText = arshia.results[state];
 candidate2Name.innerText = arshia.name;
-candidate2Results.innerText = candyse.results[state];
+candidate2Results.innerText = saundra.results[state];
 
 
 if (stateWinner === null) {
   winnerName.innerText = "DRAW";
 } else {
 winnerName.innerText = theStates[state].winner.name;
-}  
+}
 
 };
 
@@ -92,16 +92,16 @@ arshia.tallyVotes();
 console.log(arshia.totalVotes);
 console.log(arshia);
 
-candyse.tallyVotes();
-console.log(candyse.totalVotes);
-console.log(candyse);
+saundra.tallyVotes();
+console.log(saundra.totalVotes);
+console.log(saundra);
 
 var winner = "";
 var countryTotal = function() {
 
 
-if (arshia.totalVotes > candyse.totalVotes) {winner = arshia.name;}
-else if (arshia.totalVotes < candyse.totalVotes) {winner = candyse.name}
+if (arshia.totalVotes > saundra.totalVotes) {winner = arshia.name;}
+else if (arshia.totalVotes < saundra.totalVotes) {winner = saundra.name}
 else winner = "a tie";
 console.log("And the winner is " + winner + "!!!");
 };
@@ -111,8 +111,8 @@ countryTotal();
 var countryInfoTable = document.getElementById('countryResults');
 var row = countryInfoTable.children[0].children[0];
 
-row.children[0].innerText = candyse.name;
-row.children[1].innerText = candyse.totalVotes;
+row.children[0].innerText = saundra.name;
+row.children[1].innerText = saundra.totalVotes;
 row.children[2].innerText = arshia.name;
 row.children[3].innerText = arshia.totalVotes;
 row.children[5].innerText = winner;
